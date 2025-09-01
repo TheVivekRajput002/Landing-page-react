58de5a50f0958b2812264d51863e46aa025f7133
VivekRajput001 <vivekrajput4035200@gmail.com>
2025-09-01 22:16:02 +0530

    h

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,7 @@
 // postcss.config.mjs
-import tailwindcss from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
-
 export default {
-  plugins: [
-    tailwindcss(),      // Note the parentheses - calling it as a function
-    autoprefixer(),
-  ],
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
 }
